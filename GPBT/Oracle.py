21976a3050a63685c19b3bac2cf711c4fec9dfe4
AJSVB <45511657+AJSVB@users.noreply.github.com>
2022-12-29 21:00:14 +0100

    Update Oracle.py

--- GPBT/Oracle.py
+++ GPBT/Oracle.py
@@ -3,13 +3,20 @@ def set_iteration(algo,iteration):
   algo.space.paras["aiteration"].ub=iteration
 
 class Guesser():
+    """Used to sample the hyperspace with the tools of `hyperopt`
+    """
+
     def __init__(self, searchspace, verbose):
         self.searchspace = searchspace
+        self.string = "aiteration"
+
         self.verbose = verbose
         print(self.searchspace)
         self.algo = HEBO(searchspace)
 
+
     def repeat_good(self, trials, iteration, function, configuration):
+
         configuration = copy.deepcopy(configuration)
         configuration["aiteration"] = iteration
         print(configuration)
@@ -20,7 +27,7 @@ class Guesser():
     def compute_batch(self, trials, nb_eval, iteration, function):
         set_iteration(self.algo, iteration) 
         for i in range(nb_eval):
-         rec = self.algo.suggest(n_suggestions = 1)
+         rec = self.algo.suggest(n_suggestions = 1,fix_input = {"aiteration":iteration})
          rec1 = rec.to_dict()
          for key in rec1:
           rec1[key] = rec1[key][list(rec1[key].keys())[0]] 
