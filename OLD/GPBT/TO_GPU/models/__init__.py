154d31b3d17c72624b801af61c83efb682337d69
Kevin Blin <kblin@outlook.com>
2023-01-06 23:04:13 +0100

    delete old files

--- OLD/GPBT/TO_GPU/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The models subpackage contains definitions for the following model
-architecture:
-.. SimpleNet(V1): https://arxiv.org/abs/1608.06037
-"""
-
-
-from .simplenet import simplenet
--- OLD/HEBO/archived_submissions/hebo/README.md
+++
@@ -1,3 +0,0 @@
-# README
-
-Archived submission of the winning submission to [NeurIPS 2020 Black-Box Optimisation Challenge](https://bbochallenge.com/leaderboard). 
